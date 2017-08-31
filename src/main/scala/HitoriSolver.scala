b07edcc8b9001aa354d9a045869b0a48332d1696
Eivind Hystad <eivinh14@student.uia.no>
2017-08-31 15:10:08 +0200

    New project with 2D arrays! :D

---
+++ src/main/scala/HitoriSolver.scala
@@ -0,0 +1,24 @@
+object HitoriSolver {
+
+  def main(args: Array[String]): Unit = {
+
+    /*
+    * TODO Get board from file
+    * TODO Apply starting techniques
+    * TODO Check if solved
+    * TODO Continously apply techniques
+    * TODO Actually solve it (recursion with backtracking)
+    * TODO Save solution
+    * */
+
+    val board = List(List(2))(2)
+
+    val size = board.length
+
+    /* Solve function
+    *
+    * */
+
+  }
+
+}
